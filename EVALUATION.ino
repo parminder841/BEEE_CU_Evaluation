87e5774754c5a157a2608a3b5798c33d511039c0
parminder841 <55436590+parminder841@users.noreply.github.com>
2019-10-22 17:58:57 +0530

    CODE FILE

---
+++ EVALUATION.ino
@@ -0,0 +1,27 @@
+int last=LOW;
+int x;
+void setup()
+{ 
+  pinMode(2,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(8,INPUT); 
+        
+}
+
+void loop()
+{
+  int read= digitalRead(8);
+  if(read!=last)
+  {
+  		digitalWrite(4,HIGH);
+  }
+    else{
+    digitalWrite(4,LOW);
+    digitalWrite(2,HIGH);
+      delay(50);
+      digitalWrite(2,LOW);
+      delay(50);
+    }
+  
+  
+}
